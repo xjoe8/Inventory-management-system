f82f37ed284e73cb8d73cca462540250e6ade0b2
belsaqqa <b.elsaqqa@gmail.com>
2025-10-17 15:18:39 +0300

    Products class added

---
+++ lab4/src/lab4/Product.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4;
+
+/**
+ *
+ * @author basma
+ */
+public class Product {
+    String productID;
+    String productName;
+    String manufacturerName;
+    String supplierName;
+    private int quantity;
+    private float price;
+    
+    public Product(String productID, String productName, String manufacturerName, String supplierName, int quantity, float price){
+        this.productID = productID;
+        this.productName = productName;
+        this.manufacturerName = manufacturerName;
+        this.supplierName = supplierName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    public void setProductID(String productID) {
+        this.productID = productID;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getManufacturerName() {
+        return manufacturerName;
+    }
+
+    public void setManufacturerName(String manufacturerName) {
+        this.manufacturerName = manufacturerName;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+    
+    
+    public String lineRepresentation(){
+        return productID + "," + productName + "," + manufacturerName + "," + supplierName + "," + "quantity" + "," + "price";
+    }
+    
+    
+    
+}
+
