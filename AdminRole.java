a215193832fd87ac90ca9e4e74b6fc8c2af2dac6
ChrisNader <chrisnader25@gmail.com>
2025-10-19 20:52:53 +0300

    Update AdminRole.java

--- AdminRole.java
+++ AdminRole.java
@@ -1,16 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab4;
 
 import java.io.*;
 import java.util.*;
 
-/**
- *
- * @author User
- */
 public class AdminRole {
     
     private EmployeeUserDatabase database;
