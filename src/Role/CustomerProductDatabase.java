316f64869f883adbee1841422c1edc8841a55af4
ChrisNader <chrisnader25@gmail.com>
2025-10-19 20:56:23 +0300

    Update CustomerProductDatabase.java

--- src/Role/CustomerProductDatabase.java
+++ src/Role/CustomerProductDatabase.java
@@ -1,78 +1,24 @@
-package Role;
-import java.io.*;
+package lab4;
 import java.time.LocalDate;
-import java.util.ArrayList;
-public class CustomerProductDatabase {
-    private ArrayList<CustomerProduct> records = new ArrayList<CustomerProduct>();
-    private String fileName;
+import java.time.format.DateTimeFormatter;
+
+public class CustomerProductDatabase extends Database<CustomerProduct>{
+    
+    private static final DateTimeFormatter Formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+
     public CustomerProductDatabase(String fileName){
-        this.fileName = fileName;
+        super(fileName);
     }
-    public void readFromFile(){
-        try(BufferedReader rd = new BufferedReader(new FileReader(this.fileName))){
-            String line;
-            int numOfRecords = Integer.parseInt(rd.readLine());
-            for (int i= 0; i<numOfRecords; i++){
-                line = rd.readLine();
-                String[] parts = line.split(",\\s*");
-                String customerSSN = parts[0];
-                String productID = parts[1];
-                LocalDate purchaseDate = LocalDate.parse(parts[2]);
-                CustomerProduct record = new CustomerProduct(customerSSN, productID, purchaseDate);
-                record.setPaid(Boolean.parseBoolean(parts[3]));
-                this.records.add(record);
-            }
-            rd.close();
-        } catch (FileNotFoundException e){
-            System.out.println("File not Found.");
-        } catch (IOException e){
-            System.out.println("Error in reading file.");
-        }
-    }
+    
+    @Override
     public CustomerProduct createRecordFrom(String line){
         String[] parts = line.split(",\\s*");
-        return new CustomerProduct(parts[0], parts[1], LocalDate.parse(parts[2]));
+        if (parts.length < 4) 
+            return null;
+        LocalDate date = LocalDate.parse(parts[2].trim(),Formatter);
+        CustomerProduct record = new CustomerProduct(parts[0].trim(), parts[1].trim(), date);
+        record.setPaid(Boolean.valueOf(parts[3].trim()));
+        return record;
     }
-    public ArrayList<CustomerProduct> returnAllRecords(){
-        return this.records;
-    }
-    public boolean contains(String key){
-        for (CustomerProduct cp: this.records){
-            if (cp.lineRepresentation().contains(key)){
-                return true;
-            }
-        }
-        return false;
-    }
-    public CustomerProduct getRecord(String key){
-        for(CustomerProduct cp: this.records){
-            if(cp.lineRepresentation().contains(key)){
-                return new CustomerProduct(cp.getCustomerSSN(), cp.getProductID(), cp.getPurchaseDate());
-            }
-        }
-        return null;
-    }
-    public void insertRecord(CustomerProduct record){
-        this.records.add(record);
-    }
-    public void deleteRecord(String key){
-        CustomerProduct record = getRecord(key);
-        this.records.remove(record);
-    }
-    public void saveToFile(){
-        try(BufferedWriter wr = new BufferedWriter(new FileWriter(this.fileName, false))){
-            String numOfRecords = Integer.toString(this.records.size());
-            wr.write(numOfRecords);
-            wr.newLine();
-            for (int i=0; i<this.records.size(); i++){
-                String line = this.records.get(i).lineRepresentation();
-                wr.write(line);
-                wr.newLine();
-            }
-        }catch (FileNotFoundException e){
-            System.out.println("File Location error.");
-        }catch (IOException e){
-            System.out.println("Error in writing file.");
-        }
-    }
+    
 }
