91d326175c1be50c809dd52f94e05ac01fb0963b
ChrisNader <chrisnader25@gmail.com>
2025-10-19 20:53:28 +0300

    Update EmployeeUser.java

--- EmployeeUser.java
+++ EmployeeUser.java
@@ -1,18 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package lab4;
 
-/**
- *
- * @author User
- */
-public class EmployeeUser {
+public class EmployeeUser implements Storable {
     
     private String employeeId, name, email, address, phoneNumber;
     
-    //Constructor
+    
     public EmployeeUser(String employeeId, String name, String email, String address, String phoneNumber){
         this.employeeId = employeeId;
         this.name = name;
@@ -21,11 +13,13 @@ public class EmployeeUser {
         this.phoneNumber = phoneNumber;
     }
     
-    public String lineRepresentation(){ //returns the data of the employee comma separated
+    @Override
+    public String lineRepresentation(){ 
          return this.employeeId + "," + this.name + "," + this.email + "," + this.address + "," + this.phoneNumber;
     }
     
-    public String getSearchKey(){  //returns the employee id
+    @Override
+    public String getSearchKey(){ 
           return this.employeeId;
     }
 }
